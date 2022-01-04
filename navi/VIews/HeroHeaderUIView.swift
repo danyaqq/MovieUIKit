d180bb63920921a29841fe464bdba435f6660cd8
danyaqq <fedak220@gmail.com>
2022-01-04 03:37:28 +0300

    Create header view

--- navi/VIews/HeroHeaderUIView.swift
+++ navi/VIews/HeroHeaderUIView.swift
@@ -9,7 +9,26 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
 
-    
+    private let playButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 6
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private let downloadButton: UIButton = {
+       let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.titleLabel?.textColor = UIColor.red
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        button.backgroundColor = UIColor.red
+        button.layer.cornerRadius = 6
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     private let heroImageView: UIImageView = {
         let image = UIImageView()
         image.contentMode = .scaleAspectFill
@@ -18,12 +37,37 @@ class HeroHeaderUIView: UIView {
         return image
     }()
     
+    private func addGradient(){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.systemBackground.cgColor]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(heroImageView)
+        addGradient()
+        addSubview(playButton)
+        addSubview(downloadButton)
+        applyConstraints()
     }
 
+    private func applyConstraints(){
+        let playButtonConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 75),
+            playButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -75),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -100),
+        ]
+        
+        let downloadButtonConstraints = [
+            downloadButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 75),
+            downloadButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -75),
+            downloadButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -50)
+        ]
+        NSLayoutConstraint.activate(playButtonConstraints)
+        NSLayoutConstraint.activate(downloadButtonConstraints)
+    }
     override func layoutSubviews() {
         super.layoutSubviews()
         heroImageView.frame = bounds
