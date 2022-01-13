cf46bfa1c1732466f1cc1b49f6ff454dc8bd96b2
Student <student@WSR402-06.local>
2022-01-13 15:25:11 +0300

    Some fix

--- navi/Controllers/Core/MainTabBarViewController.swift
+++ navi/Controllers/Core/MainTabBarViewController.swift
@@ -17,8 +17,8 @@ class MainTabBarViewController: UITabBarController {
         let vc3 = UINavigationController(rootViewController: SearchViewController())
         let vc4 = UINavigationController(rootViewController: DownloadsViewController())
         
-        vc1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), selectedImage: nil)
-        vc2.tabBarItem = UITabBarItem(title: "Coming", image: UIImage(systemName: "play.circle"), selectedImage: nil)
+        vc1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house.fill"), selectedImage: nil)
+        vc2.tabBarItem = UITabBarItem(title: "Coming", image: UIImage(systemName: "play.circle.fill"), selectedImage: nil)
         vc3.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), selectedImage: nil)
         vc4.tabBarItem = UITabBarItem(title: "Downloads", image: UIImage(systemName: "arrow.down.to.line"), selectedImage: nil)
         
---
+++ navi/Service/NetworkService.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkService.swift
+//  navi
+//
+//  Created by Student on 13.01.2022.
+//
+
+import Foundation
+import SwiftUI
+
+class NetworkService{
+    
+}
